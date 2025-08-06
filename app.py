fcd8b3f3193d3093b719d9ae1a9e363715edbc61
clara4795 <junghyeon4795@gmail.com>
2025-08-06 14:53:31 +0900

    env로 db url 옮기

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ load_dotenv()
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mybooks.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 
 db.init_app(app)
 
